4aad857192ee2b3842f7bb8079288a96bf48ceeb
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-03 23:50:21 +0200

    move listen code into listen.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/listen.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+// listen listens on network the interface and parse packets
+func listen() {
+	/* convert pcap parameters from command line arguments */
+	_pcapTimeout := time.Duration(pcapTimeout) * time.Second
+	_pcapSnaplen := int32(pcapSnaplen)
+
+	/* open device */
+	pcapHandle, pcapErr = pcap.OpenLive(pcapDevice, _pcapSnaplen,
+		pcapPromisc, _pcapTimeout)
+	if pcapErr != nil {
+		log.Fatal(pcapErr)
+	}
+	defer pcapHandle.Close()
+
+	if httpListen != "" {
+		// start http server and print device table to clients
+		http.HandleFunc("/", printHttp)
+		go http.ListenAndServe(httpListen, nil)
+	} else {
+		// print device table periodically to console
+		go printConsole()
+	}
+
+	/* Use the handle as a packet source to process all packets */
+	packetSource := gopacket.NewPacketSource(pcapHandle,
+		pcapHandle.LinkType())
+	for packet := range packetSource.Packets() {
+		/* lock devices */
+		devicesLock.Lock()
+
+		/* parse packet */
+		parseSrcMac(packet)
+		parsePeers(packet)
+		parseVlan(packet)
+		parseVxlan(packet)
+		parseGeneve(packet)
+		parseArp(packet)
+		parseNdp(packet)
+		parseIgmp(packet)
+		parseMld(packet)
+		parseDhcp(packet)
+		parseStp(packet)
+		parsePlc(packet)
+		updateStatistics(packet)
+
+		/* unlock devices */
+		devicesLock.Unlock()
+	}
+}
--- internal/cmd/listnd.go
+++ internal/cmd/listnd.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -12,7 +11,6 @@ import (
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
 )
 
 /* variable definitions */
@@ -1015,60 +1013,4 @@ func printConsole() {
 // printHttp prints the device table to http clients
 func printHttp(w http.ResponseWriter, r *http.Request) {
 	printDevices(w)
-}
-
-/*
- ************
- *** MAIN ***
- ************
- */
-
-/* listen on network interface and parse packets */
-func listen() {
-	/* convert pcap parameters from command line arguments */
-	_pcapTimeout := time.Duration(pcapTimeout) * time.Second
-	_pcapSnaplen := int32(pcapSnaplen)
-
-	/* open device */
-	pcapHandle, pcapErr = pcap.OpenLive(pcapDevice, _pcapSnaplen,
-		pcapPromisc, _pcapTimeout)
-	if pcapErr != nil {
-		log.Fatal(pcapErr)
-	}
-	defer pcapHandle.Close()
-
-	if httpListen != "" {
-		// start http server and print device table to clients
-		http.HandleFunc("/", printHttp)
-		go http.ListenAndServe(httpListen, nil)
-	} else {
-		// print device table periodically to console
-		go printConsole()
-	}
-
-	/* Use the handle as a packet source to process all packets */
-	packetSource := gopacket.NewPacketSource(pcapHandle,
-		pcapHandle.LinkType())
-	for packet := range packetSource.Packets() {
-		/* lock devices */
-		devicesLock.Lock()
-
-		/* parse packet */
-		parseSrcMac(packet)
-		parsePeers(packet)
-		parseVlan(packet)
-		parseVxlan(packet)
-		parseGeneve(packet)
-		parseArp(packet)
-		parseNdp(packet)
-		parseIgmp(packet)
-		parseMld(packet)
-		parseDhcp(packet)
-		parseStp(packet)
-		parsePlc(packet)
-		updateStatistics(packet)
-
-		/* unlock devices */
-		devicesLock.Unlock()
-	}
 }
