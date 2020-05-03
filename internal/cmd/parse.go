3a6faac3334f5183699aa3d3876e853a7284cdde
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-04 00:33:05 +0200

    update comments in parse.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/parse.go
+++ internal/cmd/parse.go
@@ -5,58 +5,58 @@ import (
 	"github.com/google/gopacket/layers"
 )
 
-/* helper for getting src and dst mac addresses of packet */
+// getMacs is a helper for getting src and dst mac addresses of packet
 func getMacs(packet gopacket.Packet) (gopacket.Endpoint, gopacket.Endpoint) {
 	var linkSrc, linkDst gopacket.Endpoint
 
 	if link := packet.LinkLayer(); link != nil {
-		/* extract MAC addresses */
+		// extract MAC addresses
 		linkSrc, linkDst = link.LinkFlow().Endpoints()
 	}
 
 	return linkSrc, linkDst
 }
 
-/* helper for getting src and dst ip addresses of packet */
+// getIps is a helper for getting src and dst ip addresses of packet
 func getIps(packet gopacket.Packet) (gopacket.Endpoint, gopacket.Endpoint) {
 	var netSrc, netDst gopacket.Endpoint
 
 	if net := packet.NetworkLayer(); net != nil {
-		/* extract IP addresses */
+		// extract IP addresses
 		netSrc, netDst = net.NetworkFlow().Endpoints()
 	}
 
 	return netSrc, netDst
 }
 
-/* update statistics */
+// updateStatistics updates statistics
 func updateStatistics(packet gopacket.Packet) {
-	/* get addresses */
+	// get addresses
 	linkSrc, linkDst := getMacs(packet)
 	netSrc, netDst := getIps(packet)
 
-	/* increase packet counters */
+	// increase packet counters
 	packets++
 	if device := devices[linkSrc]; device != nil {
 		timestamp := packet.Metadata().Timestamp
 
-		/* mac/device */
+		// mac/device
 		device.packets++
 		device.setTimestamp(timestamp)
 
-		/* ip */
+		// ip
 		if ip := device.ips[netSrc]; ip != nil {
 			ip.packets++
 			ip.setTimestamp(timestamp)
 		}
 
-		/* mac peers */
+		// mac peers
 		if peer := device.macPeers[linkDst]; peer != nil {
 			peer.packets++
 			peer.setTimestamp(timestamp)
 		}
 
-		/* ip peers */
+		// ip peers
 		if peer := device.ipPeers[netDst]; peer != nil {
 			peer.packets++
 			peer.setTimestamp(timestamp)
@@ -64,13 +64,13 @@ func updateStatistics(packet gopacket.Packet) {
 	}
 }
 
-/* parse the source MAC address and add it to device table */
+// parseSrcMac parses the source MAC address and adds it to device table
 func parseSrcMac(packet gopacket.Packet) {
 	linkSrc, _ := getMacs(packet)
 	devices.add(linkSrc)
 }
 
-/* parse peer addresses and add them to device table */
+// parsePeers parses peer addresses and adds them to device table
 func parsePeers(packet gopacket.Packet) {
 	if !withPeers {
 		return
@@ -82,7 +82,7 @@ func parsePeers(packet gopacket.Packet) {
 	devices[linkSrc].addPeer(netDst)
 }
 
-/* parse VLAN tags */
+// parseVlan parses VLAN tags
 func parseVlan(packet gopacket.Packet) {
 	vlanLayer := packet.Layer(layers.LayerTypeDot1Q)
 	if vlanLayer != nil {
@@ -96,7 +96,7 @@ func parseVlan(packet gopacket.Packet) {
 	}
 }
 
-/* parse VXLAN header */
+// parseVxlan parses VXLAN headers
 func parseVxlan(packet gopacket.Packet) {
 	vxlanLayer := packet.Layer(layers.LayerTypeVXLAN)
 	if vxlanLayer != nil {
@@ -112,7 +112,7 @@ func parseVxlan(packet gopacket.Packet) {
 	}
 }
 
-/* parse Geneve header */
+// parseGeneve parses Geneve headers
 func parseGeneve(packet gopacket.Packet) {
 	geneveLayer := packet.Layer(layers.LayerTypeGeneve)
 	if geneveLayer != nil {
@@ -126,38 +126,38 @@ func parseGeneve(packet gopacket.Packet) {
 	}
 }
 
-/* parse ARP packets */
+// parseArp parses ARP packets
 func parseArp(packet gopacket.Packet) {
 	arpLayer := packet.Layer(layers.LayerTypeARP)
 	if arpLayer != nil {
 		arp, _ := arpLayer.(*layers.ARP)
 
-		/* arp request or reply */
+		// arp request or reply
 		switch arp.Operation {
 		case layers.ARPRequest:
 			debug("ARP Request")
 		case layers.ARPReply:
 			debug("ARP Reply")
 		}
-		/* get addresses */
+		// get addresses
 		linkSrc := layers.NewMACEndpoint(arp.SourceHwAddress)
 		netSrc := layers.NewIPEndpoint(arp.SourceProtAddress)
 
-		/* add to table */
+		// add to table
 		devices.addMacIP(linkSrc, netSrc)
 	}
 }
 
-/* parse neighbor discovery protocol packets */
+// parseNdp parses neighbor discovery protocol packets
 func parseNdp(packet gopacket.Packet) {
 	nsolLayer := packet.Layer(layers.LayerTypeICMPv6NeighborSolicitation)
 	if nsolLayer != nil {
 		debug("Neighbor Solicitation")
-		/* neighbor solicitation, get src mac and src ip */
+		// neighbor solicitation, get src mac and src ip
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
 
-		/* add to table */
+		// add to table
 		devices.addMacIP(linkSrc, netSrc)
 
 		return
@@ -166,12 +166,12 @@ func parseNdp(packet gopacket.Packet) {
 	nadvLayer := packet.Layer(layers.LayerTypeICMPv6NeighborAdvertisement)
 	if nadvLayer != nil {
 		debug("Neighbor Advertisement")
-		/* neighbor advertisement, get src mac and target ip */
+		// neighbor advertisement, get src mac and target ip
 		adv, _ := nadvLayer.(*layers.ICMPv6NeighborAdvertisement)
 		targetIP := layers.NewIPEndpoint(adv.TargetAddress)
 		linkSrc, _ := getMacs(packet)
 
-		/* add to table */
+		// add to table
 		devices.addMacIP(linkSrc, targetIP)
 
 		return
@@ -180,11 +180,11 @@ func parseNdp(packet gopacket.Packet) {
 	rsolLayer := packet.Layer(layers.LayerTypeICMPv6RouterSolicitation)
 	if rsolLayer != nil {
 		debug("Router Solicitation")
-		/* router solicitation, get src mac and src ip */
+		// router solicitation, get src mac and src ip
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
 
-		/* add to table */
+		// add to table
 		devices.addMacIP(linkSrc, netSrc)
 
 		return
@@ -193,19 +193,19 @@ func parseNdp(packet gopacket.Packet) {
 	radvLayer := packet.Layer(layers.LayerTypeICMPv6RouterAdvertisement)
 	if radvLayer != nil {
 		debug("Router Advertisement")
-		/* router advertisement, get src mac and src ip */
+		// router advertisement, get src mac and src ip
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
 
-		/* add to table */
+		// add to table
 		devices.addMacIP(linkSrc, netSrc)
 
-		/* mark device as a router */
+		// mark device as a router
 		timestamp := packet.Metadata().Timestamp
 		devices[linkSrc].router.enable()
 		devices[linkSrc].router.setTimestamp(timestamp)
 
-		/* flush prefixes and refill with advertised ones */
+		// flush prefixes and refill with advertised ones
 		adv, _ := radvLayer.(*layers.ICMPv6RouterAdvertisement)
 		devices[linkSrc].router.clearPrefixes()
 		for i := range adv.Options {
@@ -219,52 +219,52 @@ func parseNdp(packet gopacket.Packet) {
 	}
 }
 
-/* parse igmp packets */
+// parseIgmp parses igmp packets
 func parseIgmp(packet gopacket.Packet) {
 	igmpLayer := packet.Layer(layers.LayerTypeIGMP)
 	if igmpLayer == nil {
-		/* no igmp message, stop */
+		// no igmp message, stop
 		return
 	}
 	linkSrc, _ := getMacs(packet)
 
-	/* add source IP to device */
+	// add source IP to device
 	netSrc, _ := getIps(packet)
 	devices[linkSrc].addIP(netSrc)
 
-	/* igmp v1 or v2 */
+	// igmp v1 or v2
 	if igmp, ok := igmpLayer.(*layers.IGMPv1or2); ok {
-		/* parse message type */
+		// parse message type
 		switch igmp.Type {
 		case layers.IGMPMembershipQuery:
 			debug("IGMPv1or2 Membership Query")
-			/* queries are sent by routers, mark as router */
+			// queries are sent by routers, mark as router
 			devices[linkSrc].router.enable()
 			devices[linkSrc].router.setTimestamp(
 				packet.Metadata().Timestamp)
 		case layers.IGMPMembershipReportV1:
 			debug("IGMPv1 Membership Report")
-			/* add IP */
+			// add IP
 			devices[linkSrc].addIP(layers.NewIPEndpoint(
 				igmp.GroupAddress))
 		case layers.IGMPMembershipReportV2:
 			debug("IGMPv2 Membership Report")
-			/* add IP */
+			// add IP
 			devices[linkSrc].addIP(layers.NewIPEndpoint(
 				igmp.GroupAddress))
 		case layers.IGMPLeaveGroup:
 			debug("IGMPv1or2 Leave Group")
-			/* remove IP */
+			// remove IP
 			devices[linkSrc].delIP(layers.NewIPEndpoint(
 				igmp.GroupAddress))
 		}
 	}
 
-	/* igmp v3 */
+	// igmp v3
 	if igmp, ok := igmpLayer.(*layers.IGMP); ok {
 		if igmp.Type == layers.IGMPMembershipQuery {
 			debug("IGMPv3 Membership Query")
-			/* queries are sent by routers, mark as router */
+			// queries are sent by routers, mark as router
 			devices[linkSrc].router.enable()
 			devices[linkSrc].router.setTimestamp(
 				packet.Metadata().Timestamp)
@@ -272,16 +272,16 @@ func parseIgmp(packet gopacket.Packet) {
 
 		if igmp.Type == layers.IGMPMembershipReportV3 {
 			debug("IGMPv3 Membership Report")
-			/* parse multicast addresses and add/remove them */
+			// parse multicast addresses and add/remove them
 			for _, v := range igmp.GroupRecords {
 				switch v.Type {
 				case layers.IGMPIsEx, layers.IGMPToEx:
-					/* add IP */
+					// add IP
 					devices[linkSrc].addIP(
 						layers.NewIPEndpoint(
 							v.MulticastAddress))
 				case layers.IGMPIsIn, layers.IGMPToIn:
-					/* remove IP */
+					// remove IP
 					devices[linkSrc].delIP(
 						layers.NewIPEndpoint(
 							v.MulticastAddress))
@@ -291,18 +291,19 @@ func parseIgmp(packet gopacket.Packet) {
 	}
 }
 
-/* parse mld packets */
+// mld packet constants
 const mldv2IsEx = layers.MLDv2MulticastAddressRecordTypeModeIsExcluded
 const mldv2ToEx = layers.MLDv2MulticastAddressRecordTypeChangeToExcludeMode
 const mldv2IsIn = layers.MLDv2MulticastAddressRecordTypeModeIsIncluded
 const mldv2ToIn = layers.MLDv2MulticastAddressRecordTypeChangeToIncludeMode
 
+// parseMld parses mld packets
 func parseMld(packet gopacket.Packet) {
-	/* MLDv1 */
+	// MLDv1
 	qlv1 := packet.Layer(layers.LayerTypeMLDv1MulticastListenerQuery)
 	if qlv1 != nil {
 		debug("MLDv1 Query Message")
-		/* queries are sent by routers, mark as router */
+		// queries are sent by routers, mark as router
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
 		devices[linkSrc].addIP(netSrc)
@@ -315,7 +316,7 @@ func parseMld(packet gopacket.Packet) {
 	dlv1 := packet.Layer(layers.LayerTypeMLDv1MulticastListenerDone)
 	if dlv1 != nil {
 		debug("MLDv1 Done Message")
-		/* parse and remove multicast address */
+		// parse and remove multicast address
 		done, _ := dlv1.(*layers.MLDv1MulticastListenerDoneMessage)
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
@@ -328,7 +329,7 @@ func parseMld(packet gopacket.Packet) {
 	rlv1 := packet.Layer(layers.LayerTypeMLDv1MulticastListenerReport)
 	if rlv1 != nil {
 		debug("MLDv1 Report Message")
-		/* parse and add multicast address */
+		// parse and add multicast address
 		report, _ := rlv1.(*layers.MLDv1MulticastListenerReportMessage)
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
@@ -338,11 +339,11 @@ func parseMld(packet gopacket.Packet) {
 		return
 	}
 
-	/* MLDv2 */
+	// MLDv2
 	qlv2 := packet.Layer(layers.LayerTypeMLDv2MulticastListenerQuery)
 	if qlv2 != nil {
 		debug("MLDv2 Query Message")
-		/* queries are sent by routers, mark as router */
+		// queries are sent by routers, mark as router
 		linkSrc, _ := getMacs(packet)
 		netSrc, _ := getIps(packet)
 		devices[linkSrc].addIP(netSrc)
@@ -360,15 +361,15 @@ func parseMld(packet gopacket.Packet) {
 		netSrc, _ := getIps(packet)
 		devices[linkSrc].addIP(netSrc)
 
-		/* parse multicast addresses and add/remove them */
+		// parse multicast addresses and add/remove them
 		for _, v := range report.MulticastAddressRecords {
 			switch v.RecordType {
 			case mldv2IsEx, mldv2ToEx:
-				/* add IP */
+				// add IP
 				devices[linkSrc].addIP(layers.NewIPEndpoint(
 					v.MulticastAddress))
 			case mldv2IsIn, mldv2ToIn:
-				/* remove IP */
+				// remove IP
 				devices[linkSrc].delIP(layers.NewIPEndpoint(
 					v.MulticastAddress))
 			}
@@ -377,15 +378,15 @@ func parseMld(packet gopacket.Packet) {
 	}
 }
 
-/* parse dhcp packets */
+// parseDhcp parses dhcp packets
 func parseDhcp(packet gopacket.Packet) {
-	/* DHCP v4 */
+	// DHCP v4
 	dhcpLayer := packet.Layer(layers.LayerTypeDHCPv4)
 	if dhcpLayer != nil {
 		dhcp, _ := dhcpLayer.(*layers.DHCPv4)
 		linkSrc, _ := getMacs(packet)
 
-		/* add device */
+		// add device
 		devices.add(linkSrc)
 		if dhcp.Operation == layers.DHCPOpRequest {
 			debug("DHCP Request")
@@ -393,14 +394,14 @@ func parseDhcp(packet gopacket.Packet) {
 		}
 		if dhcp.Operation == layers.DHCPOpReply {
 			debug("DHCP Reply")
-			/* mark this device as dhcp server */
+			// mark this device as dhcp server
 			devices[linkSrc].dhcp.enable()
 			devices[linkSrc].dhcp.setTimestamp(
 				packet.Metadata().Timestamp)
 		}
 	}
 
-	/* DHCP v6 */
+	// DHCP v6
 	dhcpv6Layer := packet.Layer(layers.LayerTypeDHCPv6)
 	if dhcpv6Layer != nil {
 		dhcp, _ := dhcpv6Layer.(*layers.DHCPv6)
@@ -409,7 +410,7 @@ func parseDhcp(packet gopacket.Packet) {
 		devices[linkSrc].addIP(netSrc)
 		timestamp := packet.Metadata().Timestamp
 
-		/* parse message type to determine if server or client */
+		// parse message type to determine if server or client
 		switch dhcp.MsgType {
 		case layers.DHCPv6MsgTypeSolicit:
 			debug("DHCPv6 Solicit")
@@ -417,7 +418,7 @@ func parseDhcp(packet gopacket.Packet) {
 			debug("DHCPv6 Advertise")
 		case layers.DHCPv6MsgTypeRequest:
 			debug("DHCPv6 Request")
-			/* server */
+			// server
 			devices[linkSrc].dhcp.enable()
 			devices[linkSrc].dhcp.setTimestamp(timestamp)
 		case layers.DHCPv6MsgTypeConfirm:
@@ -428,7 +429,7 @@ func parseDhcp(packet gopacket.Packet) {
 			debug("DHCPv6 Rebind")
 		case layers.DHCPv6MsgTypeReply:
 			debug("DHCPv6 Reply")
-			/* server */
+			// server
 			devices[linkSrc].dhcp.enable()
 			devices[linkSrc].dhcp.setTimestamp(timestamp)
 		case layers.DHCPv6MsgTypeRelease:
@@ -437,7 +438,7 @@ func parseDhcp(packet gopacket.Packet) {
 			debug("DHCPv6 Decline")
 		case layers.DHCPv6MsgTypeReconfigure:
 			debug("DHCPv6 Reconfigure")
-			/* server */
+			// server
 			devices[linkSrc].dhcp.enable()
 			devices[linkSrc].dhcp.setTimestamp(timestamp)
 		case layers.DHCPv6MsgTypeInformationRequest:
@@ -446,21 +447,21 @@ func parseDhcp(packet gopacket.Packet) {
 			debug("DHCPv6 Relay Forward")
 		case layers.DHCPv6MsgTypeRelayReply:
 			debug("DHCPv6 Relay Reply")
-			/* server */
+			// server
 			devices[linkSrc].dhcp.enable()
 			devices[linkSrc].dhcp.setTimestamp(timestamp)
 		}
 	}
 }
 
-/* parse stp packets */
+// parseStp parses stp packets
 func parseStp(packet gopacket.Packet) {
 	stpLayer := packet.Layer(layers.LayerTypeSTP)
 	if stpLayer != nil {
 		debug("STP packet")
 		linkSrc, _ := getMacs(packet)
 
-		/* add device and mark this device as a bridge */
+		// add device and mark this device as a bridge
 		devices.add(linkSrc)
 		devices[linkSrc].bridge.enable()
 		devices[linkSrc].bridge.setTimestamp(
@@ -468,7 +469,7 @@ func parseStp(packet gopacket.Packet) {
 	}
 }
 
-/* parse plc (power-line communication/homeplug) packets */
+// parsePlc parses plc (power-line communication/homeplug) packets
 func parsePlc(packet gopacket.Packet) {
 	ethLayer := packet.Layer(layers.LayerTypeEthernet)
 	if ethLayer != nil {
@@ -477,7 +478,7 @@ func parsePlc(packet gopacket.Packet) {
 			debug("PLC packet")
 			linkSrc, _ := getMacs(packet)
 
-			/* add device and mark this device as a powerline */
+			// add device and mark this device as a powerline
 			devices.add(linkSrc)
 			devices[linkSrc].powerline.enable()
 			devices[linkSrc].powerline.setTimestamp(
