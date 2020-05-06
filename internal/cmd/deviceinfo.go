3f73cf27071add8d61d38637d9b6238205f1b949
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-06 20:15:47 +0200

    move device info into deviceinfo.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/deviceinfo.go
@@ -0,0 +1,107 @@
+package cmd
+
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+// deviceInfo is a device found on the network
+type deviceInfo struct {
+	timeInfo
+	mac       gopacket.Endpoint
+	vlans     map[uint16]*vlanInfo
+	vxlans    map[uint32]*vxlanInfo
+	geneves   map[uint32]*geneveInfo
+	powerline powerlineInfo
+	bridge    bridgeInfo
+	dhcp      dhcpInfo
+	router    routerInfo
+	packets   int
+	ips       map[gopacket.Endpoint]*ipInfo
+	macPeers  map[gopacket.Endpoint]*ipInfo
+	ipPeers   map[gopacket.Endpoint]*ipInfo
+}
+
+// addVlan adds a vlan to a device
+func (d *deviceInfo) addVlan(vlanID uint16) {
+	// add entry if it does not exist
+	if d.vlans[vlanID] == nil {
+		debug("Adding new vlan to an entry")
+		vlan := vlanInfo{}
+		vlan.vlan = vlanID
+		d.vlans[vlanID] = &vlan
+	}
+}
+
+// addVxlan adds a vxlan to a device
+func (d *deviceInfo) addVxlan(vni uint32) {
+	// add entry if it does not et
+	if d.vxlans[vni] == nil {
+		debug("Adding new vxlan to an entry")
+		vxlan := vxlanInfo{}
+		vxlan.vxlan = vni
+		d.vxlans[vni] = &vxlan
+	}
+}
+
+// addGeneve adds a geneve to a device
+func (d *deviceInfo) addGeneve(vni uint32) {
+	// add entry if it does not exist
+	if d.geneves[vni] == nil {
+		debug("Adding new geneve to an entry")
+		geneve := geneveInfo{}
+		geneve.geneve = vni
+		d.geneves[vni] = &geneve
+	}
+}
+
+// addIP adds an ip address to a device
+func (d *deviceInfo) addIP(netAddr gopacket.Endpoint) {
+	// make sure address is valid
+	if !endpointIsValidIP(netAddr) {
+		return
+	}
+
+	// add entry if it does not exist
+	if d.ips[netAddr] == nil {
+		debug("Adding new ip to an entry")
+		ip := ipInfo{}
+		ip.ip = netAddr
+		d.ips[netAddr] = &ip
+	}
+}
+
+// delIP removes an ip address from a device
+func (d *deviceInfo) delIP(netAddr gopacket.Endpoint) {
+	// make sure address is valid
+	if !endpointIsValidIP(netAddr) {
+		return
+	}
+
+	// remove entry if it exists
+	if d.ips[netAddr] != nil {
+		debug("Deleting ip from an entry")
+		delete(d.ips, netAddr)
+	}
+}
+
+// addPeer adds a peer address to a device
+func (d *deviceInfo) addPeer(addr gopacket.Endpoint) {
+	switch addr.EndpointType() {
+	case layers.EndpointMAC:
+		if d.macPeers[addr] == nil {
+			debug("Adding new mac peer to an entry")
+			// TODO: rename to addrInfo? add macInfo?
+			ip := ipInfo{}
+			ip.ip = addr
+			d.macPeers[addr] = &ip
+		}
+	case layers.EndpointIPv4, layers.EndpointIPv6:
+		if d.ipPeers[addr] == nil {
+			debug("Adding new ip peer to an entry")
+			ip := ipInfo{}
+			ip.ip = addr
+			d.ipPeers[addr] = &ip
+		}
+	}
+}
