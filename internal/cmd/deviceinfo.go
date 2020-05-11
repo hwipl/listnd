8b6331df9aa6fcb435ff9304aa4d9262e6ba7db9
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-11 22:29:06 +0200

    add Print() method to deviceInfo
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/deviceinfo.go
+++ internal/cmd/deviceinfo.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/google/gopacket"
 )
 
@@ -21,4 +24,41 @@ type deviceInfo struct {
 	mcasts    AddrMap
 	macPeers  AddrMap
 	ipPeers   AddrMap
+}
+
+// Print prints the device to w
+func (d *deviceInfo) Print(w io.Writer) {
+	// print MAC address
+	macFmt := "MAC: %-43s (age: %.f, pkts: %d)\n"
+	fmt.Fprintf(w, macFmt, d.mac, d.getAge(), d.packets)
+
+	// print properties
+	propsHeader := "  Properties:\n"
+	if d.bridge.isEnabled() ||
+		d.dhcp.isEnabled() ||
+		d.router.isEnabled() ||
+		d.powerline.isEnabled() ||
+		d.vlans.Len() > 0 ||
+		d.vxlans.Len() > 0 ||
+		d.geneves.Len() > 0 {
+
+		// start with header
+		fmt.Fprintf(w, propsHeader)
+
+		// print device properties
+		d.bridge.Print(w)
+		d.dhcp.Print(w)
+		d.router.Print(w)
+		d.prefixes.Print(w)
+		d.powerline.Print(w)
+		d.vlans.Print(w)
+		d.vxlans.Print(w)
+		d.geneves.Print(w)
+	}
+
+	// print addresses
+	d.ucasts.Print(w)
+	d.mcasts.Print(w)
+	d.macPeers.Print(w)
+	d.ipPeers.Print(w)
 }
--- internal/cmd/print.go
+++ internal/cmd/print.go
@@ -14,34 +14,6 @@ func debug(text string) {
 	}
 }
 
-// printProperties prints device properties in device table
-func printProperties(w io.Writer, device *deviceInfo) {
-	propsHeader := "  Properties:\n"
-
-	// make sure any properties are present
-	if !device.bridge.isEnabled() &&
-		!device.dhcp.isEnabled() &&
-		!device.router.isEnabled() &&
-		!device.powerline.isEnabled() &&
-		device.vlans.Len() == 0 &&
-		device.vxlans.Len() == 0 &&
-		device.geneves.Len() == 0 {
-		return
-	}
-	// start with header
-	fmt.Fprintf(w, propsHeader)
-
-	// print device properties
-	device.bridge.Print(w)
-	device.dhcp.Print(w)
-	device.router.Print(w)
-	device.prefixes.Print(w)
-	device.powerline.Print(w)
-	device.vlans.Print(w)
-	device.vxlans.Print(w)
-	device.geneves.Print(w)
-}
-
 // printDevices prints the device table
 func printDevices(w io.Writer) {
 	devicesFmt := "===================================" +
@@ -49,7 +21,6 @@ func printDevices(w io.Writer) {
 		"Devices: %-39d (pkts: %d)\n" +
 		"===================================" +
 		"===================================\n"
-	macFmt := "MAC: %-43s (age: %.f, pkts: %d)\n"
 
 	// lock devices
 	devices.Lock()
@@ -57,16 +28,8 @@ func printDevices(w io.Writer) {
 	// start with devices header
 	fmt.Fprintf(w, devicesFmt, len(devices.m), devices.packets)
 
-	for mac, device := range devices.m {
-		// print MAC address
-		fmt.Fprintf(w, macFmt, mac, device.getAge(),
-			device.packets)
-		// print properties and ips
-		printProperties(w, device)
-		device.ucasts.Print(w)
-		device.mcasts.Print(w)
-		device.macPeers.Print(w)
-		device.ipPeers.Print(w)
+	for _, device := range devices.m {
+		device.Print(w)
 		fmt.Fprintln(w)
 	}
 
