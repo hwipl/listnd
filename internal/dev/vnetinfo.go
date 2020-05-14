a2917b561952cbce3b67b566c440e7b81873c267
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-14 12:30:09 +0200

    fix VNetInfo output
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/dev/vnetinfo.go
+++ internal/dev/vnetinfo.go
@@ -12,8 +12,8 @@ type VNetInfo struct {
 
 // String converts vnet info to a string
 func (v *VNetInfo) String() string {
-	vnetFmt := "$s: %-*d (age: %.f, pkts: %d)"
+	vnetFmt := "%s: %-*d (age: %.f, pkts: %d)"
 	padLen := 44 - len(v.Type)
-	return fmt.Sprintf(vnetFmt, padLen, v.Type, v.ID, v.Age(),
+	return fmt.Sprintf(vnetFmt, v.Type, padLen, v.ID, v.Age(),
 		v.Packets)
 }
