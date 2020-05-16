4549538c5d924fe4997b75787b74d465fc10c51e
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-16 22:54:54 +0200

    add plc_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/pkt/plc_test.go
@@ -0,0 +1,97 @@
+package pkt
+
+import (
+	"bytes"
+	"log"
+	"net"
+	"testing"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/hwipl/listnd/internal/dev"
+)
+
+func testParsePLCCreatePacket(etherType layers.EthernetType) gopacket.Packet {
+	// prepare creation of packet
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	pktBuf := gopacket.NewSerializeBuffer()
+
+	// create headers
+	ethLayer := &layers.Ethernet{
+		SrcMAC:       net.HardwareAddr{1, 2, 3, 4, 5, 6},
+		DstMAC:       net.HardwareAddr{6, 5, 4, 3, 2, 1},
+		EthernetType: etherType,
+	}
+
+	// serialize to buffer
+	err := gopacket.SerializeLayers(pktBuf, opts, ethLayer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create packet from buffer
+	pkt := gopacket.NewPacket(pktBuf.Bytes(), layers.LayerTypeEthernet,
+		gopacket.Default)
+	return pkt
+}
+
+func TestParsePLC0x88e1(t *testing.T) {
+	var buf bytes.Buffer
+	var want, got string
+
+	// set device table
+	devices = &dev.DeviceMap{}
+
+	// create and parse packet
+	parsePlc(testParsePLCCreatePacket(0x88e1))
+
+	// check output
+	devices.Print(&buf)
+	want = "=================================================" +
+		"=====================\n" +
+		"Devices: 1                                       " +
+		"(pkts: 0)\n" +
+		"=================================================" +
+		"=====================\n" +
+		"MAC: 01:02:03:04:05:06                           " +
+		"(age: -1, pkts: 0)\n" +
+		"  Properties:\n" +
+		"    Powerline: true                              " +
+		"(age: -1)\n\n"
+	got = buf.String()
+	if got != want {
+		t.Errorf("got = %s; want = %s", got, want)
+	}
+}
+
+func TestParsePLC0x8912(t *testing.T) {
+	var buf bytes.Buffer
+	var want, got string
+
+	// set device table
+	devices = &dev.DeviceMap{}
+
+	// create and parse packet
+	parsePlc(testParsePLCCreatePacket(0x8912))
+
+	// check output
+	devices.Print(&buf)
+	want = "=================================================" +
+		"=====================\n" +
+		"Devices: 1                                       " +
+		"(pkts: 0)\n" +
+		"=================================================" +
+		"=====================\n" +
+		"MAC: 01:02:03:04:05:06                           " +
+		"(age: -1, pkts: 0)\n" +
+		"  Properties:\n" +
+		"    Powerline: true                              " +
+		"(age: -1)\n\n"
+	got = buf.String()
+	if got != want {
+		t.Errorf("got = %s; want = %s", got, want)
+	}
+}
