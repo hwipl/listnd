a0be848a130afd92905a59604d54a28d1d02cb53
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-19 14:12:20 +0200

    add flush to http server
    Add the query parameter "flush=true" to the http server for clearing the
    device table after printing it.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/http.go
+++ internal/cmd/http.go
@@ -4,8 +4,13 @@ import "net/http"
 
 // handleHTTP prints the device table to http clients
 func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	flush := r.URL.Query().Get("flush")
+
 	devices.Lock()
 	devices.Print(w)
+	if flush == "true" {
+		devices.Reset()
+	}
 	devices.Unlock()
 }
 
